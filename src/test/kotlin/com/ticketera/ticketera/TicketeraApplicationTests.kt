a4fa33b3b3577ce52573ca5e3709b0f75a8dba9c
leandrodalbo <leandro.edbo@gmail.com>
2025-06-19 13:03:28 -0300

    empty project

---
+++ src/test/kotlin/com/ticketera/ticketera/TicketeraApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ticketera.ticketera
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicketeraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
